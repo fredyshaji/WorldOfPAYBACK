85f7ab0e3703c28a1ca4d16819d3a85da1f19f25
Fredy Shaji <fredy.a.ext.shaji@swedbank.se>
2022-12-03 16:26:25 +0100

    feature: Add basic files for the app

---
+++ WorldOfPAYBACKApp/WorldOfPAYBACKApp/WorldOfPAYBACKAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKAppApp.swift
+//  WorldOfPAYBACKApp
+//
+//  Created by Fredy Shaji on 2022-12-03.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
